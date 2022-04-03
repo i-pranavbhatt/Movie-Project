6798c43a7f71c43f5cd6b258d470f471f7684111
i-pranavbhatt <84443935+i-pranavbhatt@users.noreply.github.com>
2022-04-03 14:09:45 -0400

    First Django Website

--- myapp1/models.py
+++ myapp1/models.py
@@ -25,14 +25,26 @@ class movie_rating_type(models.Model):
 
 class Client(User):
     c_user = models.ForeignKey(User, related_name="+",  blank=True, null=True, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200,  null=True, blank=True)
-    locality = models.CharField(max_length=200, default='Canada', blank=True, null=True)
-    city = models.CharField(max_length=50, default='xyz', blank=True)
-    zipcode = models.CharField(max_length=50,  null=True, blank=True)
-    u_contact = models.IntegerField(null=True, blank=True, )
+    name = models.CharField(max_length=200, null=True,)
+    locality = models.CharField(max_length=200, null=True,)
+    city = models.CharField(max_length=50, null=True, )
+    zipcode = models.CharField(max_length=50, null=True,)
+    u_contact = models.IntegerField( null=True,)
 
     def __str__(slef):
-        return slef.first_name + " " + slef.last_name
+        return str(slef.id)
+
+class Customer(models.Model):
+    cus_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    c_name = models.CharField(max_length=200, null=True,)
+    c_locality = models.CharField(max_length=200, null=True,)
+    c_city = models.CharField(max_length=50, null=True, )
+    c_zipcode = models.CharField(max_length=50, null=True,)
+    c_contact = models.IntegerField( null=True,)
+
+    def __str__(slef):
+        return str(slef.id) + " " + slef.c_name + " "+slef.c_locality + " " + slef.c_city
+
 
 class movie(models.Model):
     m_name = models.CharField(max_length=200)
@@ -50,7 +62,9 @@ class movie(models.Model):
     m_img = models.ImageField(null=True, blank=True, upload_to="mimages")
     m_on = models.BooleanField(default=True)
     m_price = models.FloatField()
-    m_discount = models.FloatField( blank=True, null=True,)
+    m_discount = models.FloatField( blank=True, null=True)
+    mo_rating = models.FloatField( blank=True, null=True)
+    m_desc = models.CharField(max_length=1000, null=True)
 
     def __str__(movie):
         return movie.m_name
@@ -66,12 +80,11 @@ class m_rating(models.Model):
         return movie.m_name + "  " + "Ratings:" + m_rating.m_rate
 
 class OrderItem(models.Model):
-    movie_order = models.ForeignKey(movie, related_name='movie_order', on_delete=models.CASCADE, blank=True, null=True)
     user = models.ForeignKey(User, related_name='order_user', on_delete=models.CASCADE,  blank=True, null=True)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE, blank=True, null=True)
+    client = models.ForeignKey(Customer, on_delete=models.CASCADE, blank=True, null=True)
     movie = models.ForeignKey(movie, on_delete=models.CASCADE,  blank=True, null=True)
-    order_item = models.PositiveIntegerField(default=1)
-    status = [('0', 'Cancelled Order'), ('1', 'Placed Order'), ('2', 'Shipped Order'), ('3', 'Delieverd Order')]
+    quantity = models.PositiveIntegerField(default=1)
+    status = [('0', 'Cancelled Order'), ('1', 'Placed Order'), ('2', 'Sent Mail'), ('3', 'Activated Link')]
     order_status = models.CharField(max_length=20 , choices=status, default='1')
     order_date = models.DateTimeField(auto_now_add=True)
 
@@ -80,12 +93,20 @@ class OrderItem(models.Model):
         #return slef.client.first_name
 
     def total_price(self):
-        return movie.price * OrderItem.order_item
+        return str(movie.price * OrderItem.order_item)
+
+    @property
+    def total_cost(self):
+        return self.quantity * self.movie.m_price
 
 class cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     o_movie = models.ForeignKey(movie, on_delete=models.CASCADE)
     o_quantity = models.PositiveIntegerField(default=1)
 
-    def __str__(slef):
-        return slef.user + slef.o_movie
+    def __str__(self):
+        return str(self.id)
+
+    @property
+    def total_cost(self):
+        return self.o_quantity * self.o_movie.m_price
--- myapp1/urls.py
+++ myapp1/urls.py
@@ -18,16 +18,34 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_view
+from .form import mypasswordchangeform
 
 urlpatterns = [
     #path('', views.home, name='home'),
     path('', views.movieview.as_view(), name='home'),
-    path('signup', views.signup, name='signup'),
+    path('signup/', views.CustomerRegistrationView.as_view(), name='signup'),
     path('login', views.log_in, name='log_in'),
     path('movie_list', views.movie_list, name='movie_list'),
+    path('movie_list/<slug:data>', views.movie_list, name='movie_list1'),
+    path('bollywood/<slug:data>', views.movie_filter, name='movie_filter'),
+    path('top', views.top, name='top'),
     path('base', views.base, name='bse'),
-    path('bollywood', views.base, name='bollywood'),
-    path('hollywood', views.base, name='hollywood'),
+    path('address',views.address, name='address'),
+    path('logout/', views.user_logout, name='user_logout'),
+    path('mycart/', views.mycart, name='mycart'),
+    path('cart/', views.show_cart, name='cart'),
+    path('pluscart/', views.plus_cart, name='pluscart'),
+    path('minuscart/', views.minus_cart, name='minuscart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('paymentdone/', views.paymentdone, name='paymentdone'),
+    path('removecart/', views.removecart, name='removecart'),
+    path('orders/', views.orders, name='orders'),
+    path('passwordchangedone/', auth_view.PasswordChangeView.as_view(template_name='myapp1/passwordchangedone.html') ,name='passwordchangedone'),
+
+    path('passwordchange/', auth_view.PasswordChangeView.as_view(template_name= 'myapp1/passwordchange.html', form_class=mypasswordchangeform, success_url='/passwordchangedone/'), name='changepassword'),
+    #path('logout/', auth_view.LogoutView.as_view(), name='user_logout'),
+    path('profile/', views.user_profile.as_view(), name='user_profile'),
     path('movie_detail/<int:pk>', views.movie_detail_view.as_view(), name='movie_d')
 
     #path('logout', views.logout, name='logout'),
